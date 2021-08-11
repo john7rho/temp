b9bdab13afae9d081d8e3ba9906f4fe7c84b351f
john7rho <55637821+john7rho@users.noreply.github.com>
2021-08-11 17:42:43 -0500

    Updates

---
+++ precache-manifest.f5c8ee74fc310096de0f7330513d5045.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f9db463957e9eeae075d2758540e04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93c9a0fc82778245846e",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "5914ec81340c6725ab3e",
+    "url": "/static/css/main.b9070463.chunk.css"
+  },
+  {
+    "revision": "93c9a0fc82778245846e",
+    "url": "/static/js/2.f7a25173.chunk.js"
+  },
+  {
+    "revision": "5914ec81340c6725ab3e",
+    "url": "/static/js/main.1b4e12b6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f8a9c845925cce17b9a0ba6c0fd85151",
+    "url": "/static/media/ISEF_Quad_Chart_AAD.f8a9c845.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/john7rho.github.io/john7rho.github.io/precache-manifest.3c7694b9abc19b31df00c0c25da90e58.js"
+  "/precache-manifest.f5c8ee74fc310096de0f7330513d5045.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/john7rho.github.io/john7rho.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
